b9af06ebbf7087dd33b96a6214a3f0a264c492dc
IKE COD3 <ikechukwuokoye15@gmail.com>
2023-12-11 20:36:15 +0100

    added vercel.json

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get("/public", (req, res) =>
-  res.sendFile(__dirname + "/public/signup.html")
-);
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/signup.html"));
 
 app.post("/", (req, res) => {
   const fName = req.body.firstName;
